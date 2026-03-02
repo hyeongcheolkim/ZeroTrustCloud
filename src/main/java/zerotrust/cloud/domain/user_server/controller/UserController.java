9f8cc6aa3a821f17b2ee72eec360eca271f48e30
Hyeong Cheol Kim <hyeongcheol1753@gmail.com>
2026-03-03 01:40:12 +0900

    readme 수정

--- src/main/java/zerotrust/cloud/domain/user_server/controller/UserController.java
+++ src/main/java/zerotrust/cloud/domain/user_server/controller/UserController.java
@@ -22,7 +22,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping("/api/user")
-@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+@PreAuthorize("hasAny('USER', 'ADMIN')")
 @RequiredArgsConstructor
 public class UserController {
 
